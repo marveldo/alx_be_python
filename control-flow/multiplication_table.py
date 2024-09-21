f9dadc81d0c5df17315d71cde0a69601d7a14e43
utibe solomon <utibesolomon6@gmail.com>
2024-09-21 20:10:24 +0100

    Python Control flow

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
-number_list = [1,2,3,4,5,6,7,8,9,10]
-for range_number in number_list:
-    multiple = number * range_number
-    print(f"{number} * {range_number} = {multiple}")
+
+for numbers in range(1,11):
+    multiple = number * numbers
+    print(f"{number} * {numbers} = {multiple}")
