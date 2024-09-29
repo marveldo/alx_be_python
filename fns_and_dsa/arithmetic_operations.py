086433f0e6931220c4ee3e0d06667e85521cb823
utibe solomon <utibesolomon6@gmail.com>
2024-09-29 14:05:02 +0100

    Submit work

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,5 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
+    arguments = ["num1, num2, operation"]
     if operation == 'add':
         return num1 + num2
     elif operation == "subtract":
@@ -7,7 +8,7 @@ def perform_operation(num1,num2,operation):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0 :
-            raise ValueError('Cannot divde by zero')
+           return 'Cannot divde by zero'
         return num1/num2
     else:
-        raise ValueError("Invalid Operation its only add, subtract, multiply, divide")
+        return "Invalid Operation its only add, subtract, multiply, divide"
