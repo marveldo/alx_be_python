62aa0c146db1f7044a69ffb46b43daf2fe26e10b
utibe solomon <utibesolomon6@gmail.com>
2024-10-07 12:09:41 +0100

    Corrections

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -20,11 +20,11 @@ class Library:
 
     def add_book(self, book):
         self._books.append(book)
-    def check_out_book(self, title) :
+    def check_out_book(self, title):
         for book in self._books:
             if book.title == title and not book.is_checked_out() :
                 book.set_is_checked_out(True)
-    def return_book(self, title) :
+    def return_book(self, title):
         for book in self._books:
             if book.title == title and book.is_checked_out():
                 book.set_is_checked_out(False)
