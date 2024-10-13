f6afea094dbb119c0b3d204394a6e8783c391781
utibe solomon <utibesolomon6@gmail.com>
2024-10-13 21:58:20 +0100

    submitting

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f'Book : {self.title} by {self.author}'
+        return f'Book: {self.title} by {self.author}'
     
 class EBook(Book):
 
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f'EBook: {self.title} by {self.author}, File Size : {self.file_size} KB'
+        return f'EBook: {self.title} by {self.author}, File Size: {self.file_size}KB'
     
 class PrintBook(Book):
 
@@ -23,7 +23,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f'PrintBook : {self.title} by {self.author} , Page Count : {self.page_count}'
+        return f'PrintBook: {self.title} by {self.author} , Page Count: {self.page_count}'
 
 class Library :
 
