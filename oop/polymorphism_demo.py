1ba64db7dc648ac7037a3709f2ae119bccee3241
utibe solomon <utibesolomon6@gmail.com>
2024-10-13 21:46:55 +0100

    submitting

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+
+class Shape :
+
+    def area(self):
+        raise NotImplementedError('Method needs to be overwritten')
+    
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return f'{3.14 * self.radius * self.radius : .14f}'
+    
